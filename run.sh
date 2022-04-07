482e28f42d39d4b68d34d8fbd68dc3996b557708
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-07 14:03:35 -0500

    Reordered parameters

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@ docker stop "samba"
 docker rm "samba"
 
 # Run
-sudo docker run -it -p 139:139 -p 445:445 -d dperson/samba -p \
-  --name "samba" \
+sudo docker run -it -p 139:139 -p 445:445 -d dperson/samba --name "samba" -p \
   -u "projects;prj01" \
   -s "projects private projects;/mnt/nfs/Projects;yes;no;no;projects" \
   -w ROCKGROUP
