17a362188d83187022491ad5f61a099fa57a211a
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-07 13:49:23 -0500

    Empty change just to test push/pull

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 docker stop "samba"
 docker rm "samba"
 
+# Run
 sudo docker run -it -p 139:139 -p 445:445 -d dperson/samba -p \
     -u "projects;*prj*01" \
     -s "projects private share;/mnt/nfs/Projects;no;no;no;projects"
