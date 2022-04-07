8d3258396f6358538283fc925fab1829b827d4ee
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-07 15:03:33 -0500

    Corrected name location in run command

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker stop "samba"
 docker rm "samba"
 
 # Run
-sudo docker run -it -p 139:139 -p 445:445 -d dperson/samba --name "samba" -p \
+sudo docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
   -u "projects;prj01" \
   -s "projects private projects;/mnt/nfs/Projects;yes;no;no;projects" \
   -w ROCKGROUP
