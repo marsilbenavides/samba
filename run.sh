62e0922c46227ad0642373d4674a641ac8a1d034
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-07 13:48:17 -0500

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+docker stop "samba"
+docker rm "samba"
+
+sudo docker run -it -p 139:139 -p 445:445 -d dperson/samba -p \
+    -u "projects;*prj*01" \
+    -s "projects private share;/mnt/nfs/Projects;no;no;no;projects"
