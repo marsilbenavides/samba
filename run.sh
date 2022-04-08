76517f3c7f8877d3b6567bdf7c9e0cf7fce179ab
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 12:08:25 -0500

    Corrected -n location

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ docker rm "samba"
 docker run -it --name "samba" \
   -p 139:139 -p 445:445 \
   -v /mnt/smb/Projects:/mnt/smb/Projects \
-  -n \
   -d dperson/samba -p \
+    -n \
     -u "projects;prj01" \
     -s "Projects;/mnt/smb/Projects;yes;no;yes;projects" \
     -w "ROCKGROUP"
