9877a695a550e963a30aada055ff84fb13ac9e3f
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 12:14:07 -0500

    Removed -p

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ docker rm "samba"
 
 # Run
 docker run -it --name "samba" \
-  -p 139:139 -p 445:445 \
+  -p 139:139 \
+  -p 445:445 \
   -v /mnt/smb/Projects:/mnt/smb/Projects \
-  -d dperson/samba -p \
-    -n \
+  -d dperson/samba \
     -u "projects;prj01" \
     -s "Projects;/mnt/smb/Projects;yes;no;yes;projects" \
     -w "ROCKGROUP"
