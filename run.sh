f2c6ec3405b37946fcdf87ecaaf81161aa80bda7
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 12:02:28 -0500

    Changed name of share from nfs->smb

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ docker rm "samba"
 # Run
 docker run -it --name "samba" \
   -p 139:139 -p 445:445 \
-  -v /mnt/nfs/Projects:/mnt/nfs/Projects \
+  -v /mnt/smb/Projects:/mnt/smb/Projects \
   -d dperson/samba -p \
     -u "projects;prj01" \
-    -s "Projects;/mnt/nfs/Projects;yes;no;no;projects" \
+    -s "Projects;/mnt/smb/Projects;yes;no;no;projects" \
     -w "ROCKGROUP"
