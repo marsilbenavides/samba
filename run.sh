d64f18a797119ddfd3b6c87ce08332aa1edaced2
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 11:54:41 -0500

    Corrected volume.

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ docker stop "samba"
 docker rm "samba"
 
 # Run
-docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
+docker run -it --name "samba" \
+  -p 139:139 -p 445:445 \
   -v /mnt/nfs/Projects:/mnt/nfs/Projects \
-  -u "projects;prj01" \
-  -s "Projects;/mnt/nfs/Projects;yes;no;no;projects" \
-  -w "ROCKGROUP"
+  -d dperson/samba -p \
+    -u "projects;prj01" \
+    -s "Projects;/mnt/nfs/Projects;yes;no;no;projects" \
+    -w "ROCKGROUP"
