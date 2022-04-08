63ebdf2190d14310645405232bf405426f78ca5e
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 11:46:08 -0500

    Removed workgroup

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ docker rm "samba"
 # Run
 docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
   -u "projects;prj01" \
-  -s "Projects;/mnt/nfs/Projects/;yes;no;no;projects" \
-  -w ROCKGROUP
+  -s "Projects;/mnt/nfs/Projects;yes;no;no;projects"
