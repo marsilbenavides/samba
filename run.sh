4daff43bbbd84419d27ab94a3fb62c725100258f
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 11:36:37 -0500

    Changed shared projets name

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker rm "samba"
 # Run
 sudo docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
   -u "projects;prj01" \
-  -s "projects private projects;/mnt/nfs/Projects;yes;no;no;projects" \
+  -s "Projects;/mnt/nfs/Projects/;yes;no;no;projects" \
   -w ROCKGROUP
