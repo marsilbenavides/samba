1223bf9c20beed456bac4fefc7e27d4d193cd6f6
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 12:07:42 -0500

    Start the 'nmbd' daemon to advertise the shares

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ docker rm "samba"
 docker run -it --name "samba" \
   -p 139:139 -p 445:445 \
   -v /mnt/smb/Projects:/mnt/smb/Projects \
+  -n \
   -d dperson/samba -p \
     -u "projects;prj01" \
     -s "Projects;/mnt/smb/Projects;yes;no;yes;projects" \
