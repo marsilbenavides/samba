d86f52dffd1446b2c5a161fc9a0cb89a0c132511
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 11:38:02 -0500

    Removed sudo from docker call...

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker stop "samba"
 docker rm "samba"
 
 # Run
-sudo docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
+docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
   -u "projects;prj01" \
   -s "Projects;/mnt/nfs/Projects/;yes;no;no;projects" \
   -w ROCKGROUP
