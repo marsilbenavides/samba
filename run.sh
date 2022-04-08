5c1395f985a42bc260fe8637b8833abed2bb0cfe
Marsil Benavides <marsilbenavides@outlook.com>
2022-04-08 11:52:57 -0500

    Added volume

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ docker rm "samba"
 
 # Run
 docker run -it --name "samba" -p 139:139 -p 445:445 -d dperson/samba -p \
+  -v /mnt/nfs/Projects:/mnt/nfs/Projects \
   -u "projects;prj01" \
-  -s "Projects;/mnt/nfs/Projects;yes;no;no;projects"
+  -s "Projects;/mnt/nfs/Projects;yes;no;no;projects" \
+  -w "ROCKGROUP"
